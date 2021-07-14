4cb22f380dc5a279d2235f6837d3efaab78f8b3b
king-handy <a.king.life@pm.me>
2021-07-14 00:03:21 -0400

    began form validation

--- onboarding/src/App.js
+++ onboarding/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import * as yup from 'yup'
 import Form from './Form.js'
@@ -6,15 +6,29 @@ import Form from './Form.js'
 import logo from './logo.svg';
 import './App.css';
 
+
+
 const initialFormValues = {
   name: '',
   email: '',
   password: '',
   termsOfService: '',
-  
+
 }
 
+let schema = yup.object().shape({
+  name: yup.string().required('Name is required')
+  email: yup.string().email()
+})
+
 function App() {
+  const [users, setUsers] = useState({
+    name: '',
+    email: '',
+    password: '',
+    termsOfService: ''
+  })
+
   return (
     <div className="App">
       <header className="App-header">
