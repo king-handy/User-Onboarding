fdc63a159cc1e1483c69a791a3bb00f4614ed5b9
king-handy <a.king.life@pm.me>
2021-07-15 21:57:08 -0400

    form inputs

--- onboarding/src/Form.js
+++ onboarding/src/Form.js
@@ -4,6 +4,23 @@ export default function Form() {
     
     
     return (
-
+        <div>
+            <form>
+                <label type='text'>
+                    Name:
+                    <input value={form.user} name='name' type='text' onChange={handleChange} />
+                </label>
+                <label>
+                    Email:
+                    <input value={form.email} name='email' type='text' onChange={handleChange} />
+                </label>
+                <label>
+                    Password:
+                    <input value={form.password} name='password' type='text' onChange={handleChange} />
+                <label>
+                    Terms of Service:
+                    <input value={form.terms} name='terms' type='checkbox' onChange={handleChange} />
+            </form>
+        </div>
     )
 }
